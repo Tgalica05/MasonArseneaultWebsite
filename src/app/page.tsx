be03d2a4b8184fa65dd2e83a763f3a5458a48c8e
Timothy Galica <timothygalica@gmail.com>
2026-03-09 19:51:33 -0400

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "40px", fontSize: "30px" }}>
+      <strong>Mason's Website</strong>
+    </div>
+  );
+}
