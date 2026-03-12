880fb4f89098b31c93f4bf83c1a988bb9288f9e1
Timothy Galica <timothygalica@gmail.com>
2026-03-12 14:03:22 -0400

    updated graphics for home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>
+      <body className={`${inter.className}`}>
         {/* Navbar */}
         <MobileNavbar/>
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
 'use client'
+import { Inter, Rubik_Gemstones, Bungee_Shade, Rubik_Glitch_Pop, Rubik_Glitch } from "next/font/google";
+
+const title = "Relentless Wraps";
+const description = `Put info here`;
+
+const inter = Inter({ subsets: ["latin"], });
+const rubikGemstones = Rubik_Gemstones({ subsets: ["latin"], weight: "400" });
+const bungeeShade = Bungee_Shade({ subsets: ["latin"], weight: "400" });
+const rubikGlitch = Rubik_Glitch({ subsets: ["latin"], weight: "400" });
+const rubikGlitchPop = Rubik_Glitch_Pop({ subsets: ["latin"], weight: "400" });
 
 export default function Home() {
   return (
-    <div className="text-center mt-10 text-3xl">
-      <h1>Mason's Website</h1>
-      <p className="mt-4 text-lg">Welcome to my Website</p>
+    <div>
+      {/* Image ribbon */}
+      <div
+        className= "w-full h-70 bg-center bg-cover bg-no-repeat flex justify-center items-center"
+        style={{ backgroundImage: "url('/wrapBackground.png')" }}
+      >
+        <h1 className={`text-white justify-center text-6xl md:text-9xl text-center ${rubikGlitchPop.className}`}>
+          {title}
+        </h1>
+      </div>
+
+      {/* White section below */}
+      <div className="w-full bg-white py-20 px-6 text-center">
+        <p className="max-w-3xl mb-20 mx-auto text-lg">
+          {description}
+        </p>
+      </div>
     </div>
   );
 }
