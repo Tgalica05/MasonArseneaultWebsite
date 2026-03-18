c056f3591022ba39596ea0a4432560433f3304fc
Timothy Galica <timothygalica@gmail.com>
2026-03-18 16:10:13 -0400

    added tints page v1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Image from "next/image";
-import { Inter, Rubik_Gemstones, Bungee_Shade, Rubik_Glitch_Pop, Rubik_Glitch, Slabo_27px } from "next/font/google";
+import {Rubik_Glitch, Slabo_27px } from "next/font/google";
 
 const title = "Relentless Wraps";
 const heading1 = `Transform and protect your vehicle with custom wraps, tints, 
@@ -8,7 +8,8 @@ and PPF brought to you from the greater Hartford, CT area.`
 const description1 = `With our custom care, each job is tailored to you. Whether you want
 to find a new, stylish, and protective look for your car, represent your company on an entire fleet of vehicles, 
 or upgrade and personalize your storefront, we have you covered! Our professional and meticulous
-work will ensure that no detail goes unnoticed, leaving you satisfied with a polished finish for years to come.`;
+work will ensure that no detail goes unnoticed, leaving you satisfied with a polished finish for years to come. Our
+belief is that nobody is too old to play with stickers.`;
 const heading2 = `Meet the owner`
 const description2 = `Hi! I'm Mason Arseneault, an experienced professional 
 in the car wrapping and tinting industry. I take great pride in all of my jobs, and work to the best
@@ -17,11 +18,7 @@ of my abilities to ensure that my customers are satisfied with your final produc
 customer service, while also tending to all of your wrapping, tinting, & paint protection
  needs with immense care. Feel free to contact me for a quote or check out my work via social media!`
 
-const inter = Inter({ subsets: ["latin"], });
-const rubikGemstones = Rubik_Gemstones({ subsets: ["latin"], weight: "400" });
-const bungeeShade = Bungee_Shade({ subsets: ["latin"], weight: "400" });
 const rubikGlitch = Rubik_Glitch({ subsets: ["latin"], weight: "400" });
-const rubikGlitchPop = Rubik_Glitch_Pop({ subsets: ["latin"], weight: "400" });
 const slabo = Slabo_27px({ subsets: ["latin"], weight: "400" });
 
 export default function Home() {
@@ -38,10 +35,10 @@ export default function Home() {
       </div>
 
       {/* White section below */}
-      <div className="w-full bg-white mt-20 px-6">
+      <div className="w-full bg-white pt-10 px-6">
 
         {/* First flex container w/ two columns */}
-        <div className="flex flex-col md:flex-row items-center md:items-start gap-20 max-w-6xl mx-auto my-auto">
+        <div className="flex flex-col md:flex-row items-center gap-10 md:gap-20 max-w-6xl mx-auto my-auto">
           {/* Left column: Heading + description */}
           <div className={`md:w-1/2 ${slabo.className}`}>
             <h2 className={`text-black text-2xl md:text-4xl text-center font-bold mb-6`}>
@@ -55,8 +52,8 @@ export default function Home() {
           {/* Right column: container to hold image */}
           <div className="md:w-1/2">
             <Image
-              src="/Tints/TintedRed.JPEG"
-              alt="TintedRed"
+              src="/Wraps/MysteryVan.JPEG"
+              alt="MysteryVan"
               width={300}
               height={300}
               className="w-full h-auto rounded-lg shadow-lg"
@@ -65,7 +62,7 @@ export default function Home() {
         </div>
 
         {/* Second flex container w/ two columns */}
-        <div className="flex flex-col md:flex-row items-center md:items-start py-20 gap-20 max-w-6xl mx-auto">
+        <div className="flex flex-col md:flex-row items-center py-10 md:py-20 gap-10 md:gap-20 max-w-6xl mx-auto">
 
           {/* Image column */}
           <div className="md:w-1/2 order-2 md:order-1">
@@ -117,16 +114,16 @@ export default function Home() {
               {description2}
             </p>
 
-            <p className="text-lg md:text-2xl mt-5 font-bold text-center dark:text-black">
-              Phone: +1 (860) 573-9952
+            <p className="text-lg md:text-2xl mt-5 text-center dark:text-black">
+              <strong>Phone:</strong> +1 (860) 573-9952
             </p>
 
-            <p className="text-lg md:text-2xl mt-5 font-bold text-center dark:text-black">
-              Email: relentlesswrapsct@gmail.com
+            <p className="text-lg md:text-2xl mt-5 text-center dark:text-black">
+              <strong>Email:</strong> relentlesswrapsct@gmail.com
             </p>
 
-            <p className="text-lg md:text-2xl mt-5 font-bold text-center dark:text-black">
-              Social Media Tag: @relentlesswrapsct
+            <p className="text-lg md:text-2xl mt-5 text-center dark:text-black">
+              <strong>Social Media Tag:</strong> @relentlesswrapsct
             </p>
           </div>
 
--- src/app/tints/page.tsx
+++ src/app/tints/page.tsx
@@ -1,10 +1,144 @@
 'use client'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import Image from "next/image";
+import {Rubik_Glitch, Slabo_27px } from "next/font/google";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import { useRef, useState } from 'react';
+
+
+const heading1 = "Customizable Tinting Services";
+const description1 = `Experience the ultimate in style and privacy with
+ our premium tinting services. We invest in high-quality 
+ materials to enhance your vehicle's appearance while providing UV protection 
+ and reducing glare. Whether you want a sleek, dark tint or a subtle shade, 
+ we offer a range of options to suit your preferences. Elevate your driving 
+ experience with our professional tinting solutions that combine aesthetics 
+ and functionality seamlessly. Contact us today to schedule your appointment 
+ and give your vehicle the upgrade it deserves!`
+const media = [
+  { type: 'video', src: '/Tints/Copy 061E92f2-4C90-4145-A9a9-126Da1829218.mp4' },
+  { type: 'video', src: '/Tints/Img 08032.mp4' },
+  { type: 'video', src: '/Tints/Img 08102.mp4' },
+  { type: 'video', src: '/Tints/Img 08112.mp4' },
+  { type: 'video', src: '/Tints/Img 09122.mp4' },
+  { type: 'video', src: '/Tints/vid2.mp4' }
+];
+
+const rubikGlitch = Rubik_Glitch({ subsets: ["latin"], weight: "400" });
+const slabo = Slabo_27px({ subsets: ["latin"], weight: "400" });
 
 export default function tints() {
+  const [playingIndex, setPlayingIndex] = useState<number | null>(null);
+  const videoRefs = useRef<(HTMLVideoElement | null)[]>([]);
+
+  const handlePlay = (index: number) => {
+    setPlayingIndex(index);
+    const video = videoRefs.current[index];
+    if (video) video.play();
+  };
   return (
-    <div className="text-center mt-10 text-3xl">
-      <h1>Tints</h1>
-      <p className="mt-4 text-lg">More info coming soon!</p>
+    <div>
+      <div className="w-full bg-white pt-10 px-6">
+
+        {/* First flex container w/ two columns */}
+        <div className="flex flex-col md:flex-row items-center gap-10 md:gap-20 max-w-6xl mx-auto my-auto">
+          {/* Left column: Heading + description */}
+          <div className={`md:w-1/2 ${slabo.className}`}>
+            <h2 className={`text-black text-2xl md:text-4xl text-center font-bold mb-6`}>
+              {heading1}
+            </h2>
+            <p className="text-lg text-left md:text-2xl dark:text-black">
+              {description1}
+            </p>
+          </div>
+
+          {/* Right column: container to hold image */}
+          <div className="md:w-1/2">
+            <Image
+              src="/Tints/TintedRed.JPEG"
+              alt="TintedRed"
+              width={300}
+              height={300}
+              className="w-full h-auto rounded-lg shadow-lg"
+            />
+          </div>
+        </div>
+
+        {/* Services section */}
+        <section className="max-w-6xl mx-auto py-10 md:py-20 text-center">
+          <p className={`text-4xl font-bold mb-8 ${slabo.className}`}>
+            <strong>Contact us today for</strong>
+          </p>
+
+          <div className="flex flex-col md:flex-row items-center justify-center gap-10 mb-8">
+            <span className={`text-2xl md:text-3xl italic ${slabo.className}`}>Colored Tints</span>
+            <span className="hidden md:inline-block text-3xl md:text-5xl mx-2">•</span>
+            <span className={`text-2xl md:text-3xl italic ${slabo.className}`}>Standard Window Tints</span>
+            <span className="hidden md:inline-block text-3xl md:text-5xl mx-2">•</span>
+            <span className={`text-2xl md:text-3xl italic ${slabo.className}`}>Ceramic Tints</span>
+            <span className="hidden md:inline-block text-3xl md:text-5xl mx-2">•</span>
+            <span className={`text-2xl md:text-3xl italic ${slabo.className}`}>Mirrored Tints</span>
+          </div>
+
+          <p className={`text-4xl md:text-4xl font-bold ${slabo.className}`}>
+            Any shade, any color we do it <strong className="font-bold underline">ALL</strong>! Check out some of our favorite tint work:
+          </p>
+        </section>
+      </div>
+      <div className="w-full max-w-5xl mx-auto mb-10">
+      <Swiper
+        modules={[Navigation, Pagination]}
+        navigation
+        pagination={{ clickable: true }}
+        spaceBetween={10}
+        slidesPerView={1}
+        centeredSlides={true}
+        breakpoints={{
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+            centeredSlides: false
+          },
+        }}
+      >
+        {media.map((item, index) => (
+          <SwiperSlide key={index}>
+          {item.type === 'image' ? (
+            <img
+              src={item.src}
+              alt={`media-${index}`}
+              className="w-50 h-auto object-cover rounded-xl"
+            />
+          ) : (
+            <div className="relative w-full h-auto rounded-xl overflow-hidden">
+              <video
+                src={item.src}
+                className="w-2/3 h-auto object-cover rounded-xl mx-auto"
+                ref={(el) => {
+                  videoRefs.current[index] = el;
+                  return;
+                }}                
+                controls={playingIndex === index} // show controls only when playing
+                muted
+              />
+              {playingIndex !== index && (
+                <button
+                  onClick={() => handlePlay(index)}
+                  className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+                            bg-black bg-opacity-50 text-white p-4 rounded-full text-3xl"
+                >
+                  ▶
+                </button>
+              )}
+            </div>
+          )}
+        </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
     </div>
   );
 }
